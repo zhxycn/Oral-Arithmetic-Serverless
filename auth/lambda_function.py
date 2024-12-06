f94efeb8c0bb3eda366869bcd56418955ea186f8
zhxy-CN <admin@owo.cab>
2024-12-06 15:22:19 +0800

    :tada: auth: 实现用户注册与登录

---
+++ auth/lambda_function.py
@@ -0,0 +1,156 @@
+import base64
+import hashlib
+import json
+import random
+import time
+import uuid
+
+import bcrypt
+import boto3
+
+# 数据表
+AUTH_TABLE = "Oral-Arithmetic-Auth"
+SESSION_TABLE = "Oral-Arithmetic-Session"
+USER_TABLE = "Oral-Arithmetic-User"
+QUIZ_TABLE = "Oral-Arithmetic-Quiz"
+
+# 初始化 DynamoDB 资源
+dynamodb = boto3.resource("dynamodb")
+
+
+def register(email: str, nickname: str, password: str) -> None:
+    """
+    注册
+
+    :param email: 邮箱
+    :param nickname: 昵称
+    :param password: 密码
+    :raise ValueError: 参数为空或邮箱已存在
+    """
+    # 检查参数是否为空
+    if not email or not nickname or not password:
+        raise ValueError("Missing parameter")
+
+    # 定义数据表
+    auth_table = dynamodb.Table(AUTH_TABLE)
+    user_table = dynamodb.Table(USER_TABLE)
+
+    # 检查邮箱是否存在
+    if auth_table.get_item(Key={"email": email}).get("Item"):
+        raise ValueError("Email already exists")
+
+    # 对密码进行加密
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+    # 生成 UID
+    while True:
+        uid = int(str(uuid.uuid4().int)[:8])
+        data = user_table.get_item(Key={"uid": uid})
+        if "Item" in data:
+            continue
+        else:
+            break
+
+    # 存入 DynamoDB
+    auth_item = {
+        "email": email,
+        "password": hashed_password.decode("utf-8"),
+        "uid": uid,
+    }
+    auth_table.put_item(Item=auth_item)
+    user_item = {"uid": uid, "email": email, "nickname": nickname}
+    user_table.put_item(Item=user_item)
+
+
+def login(email: str, password: str) -> [str, int]:
+    """
+    登录
+
+    :param email: 邮箱
+    :param password: 密码
+    :return: 用于 Cookie 的 session 值和有效期
+    :raise ValueError: 参数为空或邮箱密码错误。
+    """
+    # 检查参数是否为空
+    if not email or not password:
+        raise ValueError("Missing parameter")
+
+    # 定义数据表
+    auth_table = dynamodb.Table(AUTH_TABLE)
+    session_table = dynamodb.Table(SESSION_TABLE)
+
+    # 获取用户数据
+    data = auth_table.get_item(Key={"email": email}).get("Item")
+
+    # 验证用户名和密码
+    if not data or not bcrypt.checkpw(
+        password.encode("utf-8"), data["password"].encode("utf-8")
+    ):
+        raise ValueError("Invalid Email or Password")
+
+    uid = data.get("uid")
+
+    # 通过 UID 与时间戳和随机数生成初始 session
+    timestamp = int(time.time())
+    random_number = str(random.randint(1000, 9999))
+    session_raw = f"{uid}{str(timestamp)}{random_number}".encode("utf-8")
+
+    # 生成 session
+    session = hashlib.sha256(session_raw).hexdigest()
+    expiration = 604800  # 有效期一周
+
+    # 将 token 存储在 DynamoDB
+    session_table.put_item(
+        Item={"session": session, "uid": uid, "expiration": timestamp + expiration}
+    )
+
+    return session, expiration
+
+
+def lambda_handler(event, context):
+    # 获取事件类型
+    try:
+        event_type = event["queryStringParameters"]["type"]
+    except KeyError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"message": "Missing parameter"}),
+        }
+
+    # 解析请求体
+    body = (
+        (
+            json.loads(base64.b64decode(event["body"]).decode("utf-8"))
+            if event.get("isBase64Encoded")
+            else event["body"]
+        )
+        if "body" in event
+        else None
+    )
+    email = body.get("email", None) if body else None
+    nickname = body.get("nickname", None) if body else None
+    password = body.get("password", None) if body else None
+
+    # 注册
+    if event_type == "register":
+        try:
+            register(email, nickname, password)
+            return {"statusCode": 201, "body": "Success"}
+        except ValueError as e:
+            return {"statusCode": 400, "body": json.dumps({"message": str(e)})}
+
+    # 登录
+    if event_type == "login":
+        try:
+            session, expiration = login(email, password)
+            return {
+                "statusCode": 201,
+                "headers": {
+                    "Set-Cookie": f"session={session}; Path=/; Max-Age={expiration}; Secure"
+                },
+                "body": "Cookie Set",
+            }
+        except ValueError as e:
+            return {"statusCode": 400, "body": json.dumps({"message": str(e)})}
+
+    return {"statusCode": 400, "body": ({"message": "Invalid parameter"})}
